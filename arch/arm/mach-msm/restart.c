abc52fce5c7ea55d6fe1e84e0894ef1ff7c0f163
Vojtech Bocek <vbocek@gmail.com>
2013-09-23 22:30:02 +0200

    Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to flo, it is based of my grouper port, which is based of
      Asus TF201 patches ported by Jens Andersen <jens.andersen@gmail.com>.
    
      I've moved atags copying from guest to the host kernel, which means there
      is no need to patch the guest kernel, assuming the --mem-min in kexec call
      is within the first 256MB of System RAM, otherwise it will take a long time
      to load. I've also fixed /proc/atags entry, which would give the kexec-tools
      userspace binary only the first 1024 bytes of atags,
      see arch/arm/kernel/atags.c for more details.
    
      Other than that, memory-reservation code for the hardboot page and
      some assembler to do the watchdog reset on MSM chip are new for this device.
    
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>
    
    Conflicts:
    	arch/arm/boot/compressed/head.S
    	arch/arm/configs/flo_defconfig
    
    Conflicts:
    	arch/arm/configs/bricked_defconfig
    	arch/arm/configs/flo_defconfig
    	arch/arm/mach-msm/asustek/devices_asustek.c
    	arch/arm/mach-msm/restart.c

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -35,6 +35,15 @@
 #include "msm_watchdog.h"
 #include "timer.h"
 
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/kexec.h>
+#endif
+
+extern unsigned get_cable_status(void);
+
+>>>>>>> 5591c31... Implement kexec-hardboot
 #define WDT0_RST	0x38
 #define WDT0_EN		0x40
 #define WDT0_BARK_TIME	0x4C
@@ -322,6 +331,14 @@ static int __init msm_pmic_restart_init(void)
 
 late_initcall(msm_pmic_restart_init);
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+static void msm_kexec_hardboot_hook(void)
+{
+	// Set PMIC to restart-on-poweroff
+	pm8xxx_reset_pwr_off(1);
+}
+#endif
+
 static int __init msm_restart_init(void)
 {
 #ifdef CONFIG_MSM_DLOAD_MODE
@@ -336,6 +353,10 @@ static int __init msm_restart_init(void)
 	msm_tmr0_base = msm_timer_get_timer0_base();
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
 	pm_power_off = msm_power_off;
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+	kexec_hardboot_hook = msm_kexec_hardboot_hook;
+#endif
 
 	return 0;
 }
